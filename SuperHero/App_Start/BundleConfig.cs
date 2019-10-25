c2c4b7b27bc7876428094f68c4563a2fd65eb92b
alexHardy872 <alexhardy872@gmail.com>
2019-10-25 08:08:19 -0500

    theme changed, image added ALL USERSTORIES MET

--- SuperHero/App_Start/BundleConfig.cs
+++ SuperHero/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SuperHero
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap2.css",
                       "~/Content/site.css",
                       "~/Content/Alex.css"));
         }
